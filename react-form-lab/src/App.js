b2155b98beeeebc97a05c87503f6dd8b278a817f
kadjiah <kadijahwilson@pursuit.org>
2019-11-26 21:04:22 -0500

    input, select and button created

--- react-form-lab/src/App.js
+++ react-form-lab/src/App.js
@@ -1,14 +1,42 @@
 import React from 'react';
 import './App.css';
 
-function App() {
+class App extends React.Component{
+constructor(){
+  super()
+this.state = {
+  inputValue: '',
+  functions: '',
+  submitted: false
+}
+}
+
+
+render(){
+  const {inputValue, functions, calculate} = this.state;
+
   return (
     <div className="App">
-      
-       
-      
+<form onSubmit={this.handleFormSubmit}>  
+      <input type="text" 
+       placeholder="enter numbers" 
+       value={inputValue}
+       onChange={this.handleInput}/>
+
+    <select id="functions" value={functions} onChange={this.handleSelectChange}>
+    <option value=""></option>
+    <option value='sum'>sum</option>
+    <option value='avg'>average</option>
+    <option value='mod'>mode</option>
+    </select>
+
+    <button>calculate</button>
+</form>
     </div>
+
   );
 }
+}
+
 
 export default App;
